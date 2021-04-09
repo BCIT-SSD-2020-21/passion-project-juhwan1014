7ffd5b89624973555d75430111dbc645cc52721b
juhwan1014 <jmoon63@my.bcit.ca>
2021-04-09 16:03:51 -0700

    add alert in login page

--- Nowflix/Controller/ViewController.swift
+++ Nowflix/Controller/ViewController.swift
@@ -6,9 +6,48 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        guard let email = emailTextField.text, let password = passwordTextField.text
+        else {
+                return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if let err = error {
+                
+                let alert = UIAlertController(title: "Please, try again", message: "Sign up Error!", preferredStyle: .alert  )
+                
+                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                
+                alert.addAction(action)
+                
+                self.present(alert, animated: true, completion: nil)
+                
+                print("Error logging in firebase user: \(err)")
+            } else{
+                
+                let alert = UIAlertController(title: "Welcome to NOWFLIX!", message: "Successfully sign in!", preferredStyle: .alert  )
+                
+                let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+                
+                alert.addAction(action)
+                
+                self.present(alert, animated: true, completion: nil)
+                
+                print("Successfully logged in user \(String(describing: user))")
+            }
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
